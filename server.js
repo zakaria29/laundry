a3fabbc9ff53cbe64d24bd55bc299f9b4b8c6c84
zakaria29 <zakaria29@smktelkom-mlg.sch.id>
2021-10-19 10:48:21 +0700

    crud users

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express()
 
 // panggil router member
 const member = require("./routers/member")
+const users = require("./routers/users")
 
-app.use("/api/member",member)
+app.use("/api/member", member)
+app.use("/api/users", users)
 
 app.listen(8000,() => {
     console.log(`Server run on port 8000`);
